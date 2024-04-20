c91dfc289641bcc91e1b894948589c0c4aa95904
Daniel Araya <darayaroma@gmail.com>
2024-04-20 15:43:45 -0600

    [ADD] Wrapped App within the LLMProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import React from "react";
 import Form from "./Form";
 import "./index.css";
+import LLMProvider from "./context/LLMContext";
 
 function App() {
   return (
-    <>
+    <LLMProvider>
       <Form />
-    </>
+    </LLMProvider>
   );
 }
 
