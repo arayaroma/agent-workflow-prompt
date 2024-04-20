0ba2b71977fdb2d9647b6723b5fa080e1c928d07
Daniel Araya <darayaroma@gmail.com>
2024-04-20 15:45:54 -0600

    [ADD] chrome.storage API for messaging from extension to webpage and inject code

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,20 +1,50 @@
-import GeminiLogo from "./GeminiLogo";
+import React, { useState } from "react";
+import LLMContainer from "./components/LLMContainer";
+import Workflows from "./components/Workflows";
+import { useLLMContext } from "./context/LLMContext";
+import { chooseWorkflow } from "./ts/workflow";
+import { chooseAgent } from "./ts/agent";
+
+export const Context = React.createContext({});
 
 const Form = () => {
+  const [form, setForm] = useState({ search: "" });
+  const { agent, workflow } = useLLMContext();
+
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setForm({ search: e.target.value });
+  };
+
+  const handleFetch = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    chrome.storage.local.set({
+      prompt: chooseWorkflow(form.search, workflow),
+      agent: agent,
+    });
+    window.open(chooseAgent(agent), "_blank");
+  };
+
   return (
-    <form className="flex justify-center items-center py-10 bg-purple-950">
-      <div className="justify-center items-center px-24 sm:px-20">
-        <GeminiLogo />
-        <h1 className="text-4xl mb-2 text-white font-bold">AI Prompt</h1>
-        <div className="flex flex-col gap-y-2">
-          <input type="text" />
-          <button
-            type="submit"
-            className="rounded bg-green-600 px-2 hover:bg-green-700 text-xl text-white py-1"
-          >
-            Search
-          </button>
-        </div>
+    <form className="flex justify-center bg-neutral-700 min-w-[400px]">
+      <div className="justify-center items-center p-4">
+        <h1 className="text-4xl mb-4 text-white font-bold text-center">
+          AI Prompt Tool
+        </h1>
+
+        <LLMContainer />
+        <textarea
+          onChange={handleInput}
+          className="w-full text-2xl rounded-md h-32 mb-2 bg-neutral-800 text-white p-2"
+        />
+
+        <Workflows />
+        <button
+          type="submit"
+          onClick={handleFetch}
+          className="rounded-md bg-purple-700 px-2 hover:bg-purple-800 text-white mt-1 py-1 text-xl font-bold w-full"
+        >
+          Search
+        </button>
       </div>
     </form>
   );
